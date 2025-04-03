ac6a6e3db30c91445d20bff3f27c86bb90b53f4b
aggarwalvivaan <vivaanaggarwal07@gmail.com>
2025-04-03 23:59:01 +0530

    added R, I and S type instructions

--- CO_045_Simulator.py
+++ CO_045_Simulator.py
@@ -47,3 +47,78 @@ def imm_extend(binary_str):   #extends immediate and converts to decimal value
     sign_bit = binary_str[0]  
     extended_bits = sign_bit * (32 - x)  
     return bin_to_decimal(extended_bits + binary_str)
+
+# R-type Instruction Class
+class Rtype:
+    def __init__(self, instruction):
+        self.instr = instruction
+        self.parts = {
+            "opcode": self.instr[25:32],
+            "rd": bin_to_decimal_u(self.instr[20:25]),
+            "funct3": self.instr[17:20],
+            "rs1": bin_to_decimal_u(self.instr[12:17]),
+            "rs2": bin_to_decimal_u(self.instr[7:12]),
+            "funct7": self.instr[0:7]
+        }
+        
+    def add(self):
+        registers[self.parts["rd"]] = registers[self.parts["rs1"]] + registers[self.parts["rs2"]]
+    
+    def sub(self):
+        registers[self.parts["rd"]] = registers[self.parts["rs1"]] - registers[self.parts["rs2"]]
+    
+    def slt(self):
+        if registers[self.parts["rs1"]] < registers[self.parts["rs2"]]:
+            registers[self.parts["rd"]] = 1
+    
+    def srl(self):
+        shift = registers[self.parts["rs2"]] & 0x1F
+        registers[self.parts["rd"]] = registers[self.parts["rs1"]] >> shift
+    
+    def or_operation(self):
+        registers[self.parts["rd"]] = registers[self.parts["rs1"]] | registers[self.parts["rs2"]]
+    def and_operation(self):
+        registers[self.parts["rd"]] = registers[self.parts["rs1"]] & registers[self.parts["rs2"]]
+
+# I-type Instruction Class
+class Itype:
+    def __init__(self, instruction):
+        self.instr = instruction
+        self.parts = {
+            "opcode": self.instr[25:32],
+            "rd": bin_to_decimal_u(self.instr[20:25]),
+            "funct3": self.instr[17:20],
+            "rs1": bin_to_decimal_u(self.instr[12:17]),
+            "imm": self.instr[0:12]
+        }
+        
+    def addi(self):
+        registers[self.parts["rd"]] = registers[self.parts["rs1"]] + imm_extend(self.parts["imm"])
+    
+    def jalr(self, pc):
+        registers[self.parts["rd"]] = pc + 4
+        if self.parts["rs1"]!=0:
+            new_pc = registers[self.parts["rs1"]] + imm_extend(self.parts["imm"])
+        else:
+            new_pc = imm_extend(self.parts["imm"])
+        return new_pc & ~1  # Ensures even jump
+    
+    def lw(self):
+        address = decimal_to_hex(registers[self.parts["rs1"]] + imm_extend(self.parts["imm"]))
+        registers[self.parts["rd"]] = memory.get(address, 0)
+
+# S-type Instruction Class
+class Stype:
+    def __init__(self, instruction):
+        self.instr = instruction
+        self.parts = {
+            "opcode": self.instr[25:32],
+            "funct3": self.instr[17:20],
+            "rs1": bin_to_decimal_u(self.instr[12:17]),
+            "rs2": bin_to_decimal_u(self.instr[7:12]),
+            "imm": self.instr[0:7] + self.instr[20:25]      
+        }
+        
+    def sw(self):
+        address = decimal_to_hex(registers[self.parts["rs1"]] + imm_extend(self.parts["imm"]))
+        memory[address] = registers[self.parts["rs2"]]
